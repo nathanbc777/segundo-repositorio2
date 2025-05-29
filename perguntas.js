1af0a7969a67f4a3625a83434ae85b66ab7b6896
nathanbc777 <nathan.castanha@escola.pr.gov.br>
2025-05-28 22:24:43 -0300

    aula 28/05 1

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao (
+    'programação',
+    'O que é uma função?',
+    'JavaScript é uma linguagem de programação'
+)
